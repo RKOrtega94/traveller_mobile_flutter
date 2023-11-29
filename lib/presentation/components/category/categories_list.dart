c2affa4099d62e62f2337048b04a084bd224b5bb
RKOrtega94 <rkortega1994@gmail.com>
2023-11-29 17:06:55 -0500

    Remove unused imports

--- lib/presentation/components/category/categories_list.dart
+++ lib/presentation/components/category/categories_list.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import 'package:traveller_mobile_flutter/data/models/category.dart';
 import 'package:traveller_mobile_flutter/presentation/components/category/category_card.dart';
 import 'package:traveller_mobile_flutter/presentation/components/skeletons/horizontal_list_skeleton.dart';
--- lib/presentation/screens/splash_screen.dart
+++ lib/presentation/screens/splash_screen.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:traveller_mobile_flutter/presentation/provider/_app_providers.dart';
-import 'package:traveller_mobile_flutter/presentation/screens/main_screen.dart';
 
 class SplashScreen extends ConsumerStatefulWidget {
   static String get routeName => "/";
